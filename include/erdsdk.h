93f30cf028f0c78f6524c7c9ea1163bbd4d8a754
Marius Ciuca <marius.ciucam@gmail.com>
2021-05-30 16:12:01 +0300

    CLN: CMake files and add erdsdk.h file

---
+++ include/erdsdk.h
@@ -0,0 +1,11 @@
+#ifndef ERDCPP_SDK_H
+#define ERDCPP_SDK_H
+
+#include "internal/internal.h"
+#include "transaction/signer.h"
+#include "transaction/transaction.h"
+#include "account/account.h"
+#include "account/address"
+
+
+#endif //ERDCPP_SDK_H
