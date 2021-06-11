dd8799a3a420a1342ea50ef8483a27cb36a00472
Marius Ciuca <marius.ciucam@gmail.com>
2021-06-11 12:06:01 +0300

    FIX: Moved all json implementation in header

--- src/wrappers/jsonwrapper.h
+++ src/wrappers/jsonwrapper.h
@@ -5,7 +5,6 @@
 
 #include "internal/internal.h"
 #include "json/json.hpp"
-//TODO: Ugly include, must take care of CMake here. Also, move implementation in CPP
 #include "../utils/errors.h"
 #include "../utils/base64.h"
 
@@ -50,13 +49,23 @@ class OrderedJson
 public:
     explicit OrderedJson() = default;
 
+    bool contains(std::string const &key) const
+    {
+        return m_json.contains(key);
+    }
+
     template <typename T>
     void set(std::string const &key, T const &value)
     {
-        internal::set(m_json, key, value);
+        try
+        {
+            internal::set(m_json, key, value);
+        }
+        catch (...)
+        {
+            throw std::invalid_argument(ERROR_MSG_JSON_SET + key);
+        }
     }
-
-    bool contains(std::string const &key) const;
 
     template <typename T>
     T at(std::string const &key) const
@@ -71,9 +80,22 @@ public:
         }
     }
 
-    std::string serialize() const;
+    std::string serialize() const
+    {
+        return m_json.dump();
+    }
 
-    void deserialize(std::string const& serialized);
+    void deserialize(std::string const& serialized)
+    {
+        try
+        {
+            m_json = nlohmann::ordered_json::parse(serialized);
+        }
+        catch (...)
+        {
+            throw std::invalid_argument(ERROR_MSG_JSON_SERIALIZED);
+        }
+    }
 
 private:
     nlohmann::ordered_json m_json;
