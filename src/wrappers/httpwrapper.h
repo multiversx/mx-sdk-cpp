928586b3dc8987a12794f47305efa3a115cb20f7
Marius Ciuca <marius.ciucam@gmail.com>
2021-06-17 18:53:40 +0300

    FEAT: Json wrapper can add/set multiple keys

--- src/wrappers/httpwrapper.h
+++ src/wrappers/httpwrapper.h
@@ -1,11 +1,64 @@
 #ifndef ERD_WRAPPER_HTTP_H
 #define ERD_WRAPPER_HTTP_H
 
+#include <string>
+#include "http/httplib.h"
+
+
 namespace http
 {
+
+struct Result
+{
+    int status;
+    bool error;
+    std::string body;
+};
+
 class Client
 {
+public:
+    explicit Client(std::string const &url): m_client(url.c_str()){}
 
+    Result get(std::string const &path)
+    {
+        std::string body;
+        int status = -1;
+        bool error = false;
+
+        m_client.Get(path.c_str());
+
+        if (auto res = m_client.Get(path.c_str()))
+        {
+            status = res->status;
+
+            if (res->status == 200)
+            {
+                body = res->body;
+            }
+        }
+        else
+        {
+            error = (res.error() != httplib::Error::Success);
+        }
+
+        return Result{status, error, body};
+    }
+
+    Result post(std::string const &path, std::string const &message)
+    {
+        auto res = m_client.Post(path.c_str(), message.c_str(), "text/plain");
+
+        return Result {res->status, res.error() != httplib::Error::Success, res->body};
+    }
+
+    std::string getStatusMessage(int const &status)
+    {
+        return  std::string(httplib::detail::status_message(status));
+    }
+
+private:
+    httplib::Client m_client;
 };
 }
 
--- src/wrappers/jsonwrapper.h
+++ src/wrappers/jsonwrapper.h
@@ -1,13 +1,12 @@
 #ifndef ERD_WRAPPER_JSON_H
 #define ERD_WRAPPER_JSON_H
 
-#include <string>
+#include <sstream>
 
 #include "internal/internal.h"
 #include "json/json.hpp"
 #include "../utils/errors.h"
 #include "../utils/base64.h"
-
 
 namespace internal
 {
@@ -66,11 +65,39 @@ public:
     {
         try
         {
-            internal::set(m_json, key, value);
+            if (!multipleKeys(key))
+            {
+                internal::set(m_json, key, value);
+            }
+            else
+            {
+                std::vector<std::string> const keys = getKeys(key);
+
+                std::string currKey;
+                nlohmann::ordered_json nestedJson;
+                uint32_t const noOfNestedJsons = keys.size() - 1;
+
+                // Example: set {key, value} = {[a][b][c], d}
+                // [a][b][c] = d
+                // json1[c] = d <=> {b:c}
+                // json2[b] = json1 <=> {b:{c:d}}
+                // json3[a] = json2 <=> {a:{b:{c:d}}}
+                internal::set(nestedJson, keys.back(), value);
+
+                for (int i = (noOfNestedJsons - 1); i >= 0; --i)
+                {
+                    currKey = keys[i];
+                    nlohmann::ordered_json const tmpJson = nestedJson;
+
+                    nestedJson.clear();
+                    nestedJson[currKey] = tmpJson;
+                }
+                m_json = nestedJson;
+            }
         }
         catch (...)
         {
-            throw std::invalid_argument(ERROR_MSG_JSON_SET + key);
+            throw std::invalid_argument(ERROR_MSG_JSON_SET_KEY + key);
         }
     }
 
@@ -79,11 +106,36 @@ public:
     {
         try
         {
-            return internal::at<T>(m_json, key);
+            if (!multipleKeys(key))
+            {
+                return internal::at<T>(m_json, key);
+            }
+            else
+            {
+                std::vector<std::string> const keys = getKeys(key);
+
+                std::string currKey;
+                auto const noOfNestedJsons = keys.size() - 1;
+
+                // Example: get {key, value} = {[a][b][c], d}
+                // [a][b][c] = d
+                // json[a] = json[b] <=> {a:{b:{c:d}}}
+                // json[b] = json[c] <=> {b:{c:d}}
+                // json[c] = d       <=> {c:d}
+                nlohmann::ordered_json tmpJson(m_json);
+
+                for (auto i = 0; i < noOfNestedJsons; i++)
+                {
+                    currKey = keys[i];
+                    tmpJson = tmpJson[currKey];
+                }
+
+                return internal::at<T>(tmpJson, keys.back());
+            }
         }
         catch (...)
         {
-            throw std::invalid_argument(ERROR_MSG_JSON_KEY_NOT_FOUND + key);
+            throw std::invalid_argument(ERROR_MSG_JSON_AT_KEY + key);
         }
     }
 
@@ -106,6 +158,26 @@ public:
     }
 
 private:
+    bool multipleKeys(std::string const& key) const
+    {
+        return key.find(kKeyDelimiter) != std::string::npos;
+    }
+
+    std::vector <std::string> getKeys(std::string const& keys) const
+    {
+        std::vector <std::string> tokens;
+        std::stringstream stream(keys);
+        std::string currToken;
+
+        while(std::getline(stream, currToken, kKeyDelimiter))
+        {
+            tokens.push_back(currToken);
+        }
+
+        return tokens;
+    }
+
+    char const kKeyDelimiter = '/';
     nlohmann::ordered_json m_json;
 };
 }
