6f11bc13b8fbdddfa46c9b691dcb587f0dd51148
Marius Ciuca <marius.ciucam@gmail.com>
2021-06-24 15:36:39 +0300

    FEAT: Finish tests for apiresponse

--- tests/test_src/test_provider/test_apiresponse.cpp
+++ tests/test_src/test_provider/test_apiresponse.cpp
@@ -21,3 +21,80 @@ TEST(ErdGenericApiResponse, constructor_invalidData)
                      }
                  }, std::invalid_argument );
 }
+
+TEST(ErdGenericApiResponse, getters_allExist)
+{
+    ErdGenericApiResponse apiResponse(R"({"data":{"account":{"address":"erd1qq"}},"error":"some error","code":"some code"})");
+
+    nlohmann::json expectedData;
+    expectedData["account"]["address"] = "erd1qq";
+
+    EXPECT_EQ(apiResponse.getCode(), "some code");
+    EXPECT_EQ(apiResponse.getError(), "some error");
+    EXPECT_EQ(apiResponse.getData<nlohmann::json>(), expectedData);
+}
+
+TEST(ErdGenericApiResponse, getters_missingCode)
+{
+    ErdGenericApiResponse apiResponse(R"({"data":{"account":{"address":"erd1qq"}},"error":"some error"})");
+
+    nlohmann::json expectedData;
+    expectedData["account"]["address"] = "erd1qq";
+
+    EXPECT_THROW({
+                     try
+                     {
+                         apiResponse.getCode();
+                     }
+                     catch(const std::invalid_argument &e)
+                     {
+                         EXPECT_EQ( ERROR_MSG_JSON_KEY_NOT_FOUND + "code", e.what() );
+                         throw;
+                     }
+                 }, std::invalid_argument );
+    EXPECT_EQ(apiResponse.getError(), "some error");
+    EXPECT_EQ(apiResponse.getData<nlohmann::json>(), expectedData);
+}
+
+TEST(ErdGenericApiResponse, getters_missingError)
+{
+    ErdGenericApiResponse apiResponse(R"({"data":{"account":{"address":"erd1qq"}},"code":"some code"})");
+
+    nlohmann::json expectedData;
+    expectedData["account"]["address"] = "erd1qq";
+
+    EXPECT_EQ(apiResponse.getCode(), "some code");
+    EXPECT_TRUE(apiResponse.getError().empty());
+    EXPECT_EQ(apiResponse.getData<nlohmann::json>(), expectedData);
+}
+
+TEST(ErdGenericApiResponse, getters_emptyError)
+{
+    ErdGenericApiResponse apiResponse(R"({"data":{"account":{"address":"erd1qq"}},"code":"some code","error":""})");
+
+    nlohmann::json expectedData;
+    expectedData["account"]["address"] = "erd1qq";
+
+    EXPECT_EQ(apiResponse.getCode(), "some code");
+    EXPECT_TRUE(apiResponse.getError().empty());
+    EXPECT_EQ(apiResponse.getData<nlohmann::json>(), expectedData);
+}
+
+TEST(ErdGenericApiResponse, getters_missingData)
+{
+    ErdGenericApiResponse apiResponse(R"({"error":"some error","code":"some code"})");
+
+    EXPECT_THROW({
+                     try
+                     {
+                         apiResponse.getData<nlohmann::json>();
+                     }
+                     catch(const std::invalid_argument &e)
+                     {
+                         EXPECT_EQ( ERROR_MSG_JSON_KEY_NOT_FOUND + "data", e.what() );
+                         throw;
+                     }
+                 }, std::invalid_argument );
+    EXPECT_EQ(apiResponse.getCode(), "some code");
+    EXPECT_EQ(apiResponse.getError(), "some error");
+}
