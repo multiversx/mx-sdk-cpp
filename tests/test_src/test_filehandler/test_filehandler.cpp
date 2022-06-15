cb9c71738541ee957b4509296acab65adb09c24c
Marius Ciuca <marius.ciucam@gmail.com>
2022-06-15 14:00:26 +0300

    FIX: Spacing

--- tests/test_src/test_filehandler/test_filehandler.cpp
+++ tests/test_src/test_filehandler/test_filehandler.cpp
@@ -67,17 +67,17 @@ INSTANTIATE_TEST_SUITE_P(
         ValidPemFiles,
         PemFileReaderParametrized,
         ::testing::Values
-        (pemData {getCanonicPath("testData/keysValid2.pem"),                                               // File path
-                  "413f42575f7f26fad3317a778771212fdb80245850981e48b58a4f25e344e8f9",        // Seed
-                  "0139472eff6886771a982f3083da5d421f24c29181e63888228dc81ca60d69e1",    // Public key
-                  "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"}, // Bech32 address
+        (pemData {getCanonicPath("testData/keysValid2.pem"),                          // File path
+                  "413f42575f7f26fad3317a778771212fdb80245850981e48b58a4f25e344e8f9", // Seed
+                  "0139472eff6886771a982f3083da5d421f24c29181e63888228dc81ca60d69e1", // Public key
+                  "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"},  // Bech32 address
 
-         pemData {getCanonicPath("testData/keysValid3.pem"),                                   // File path
+         pemData {getCanonicPath("testData/keysValid3.pem"),                            // File path
                   "b8ca6f8203fb4b545a8e83c5384da033c415db155b53fb5b8eba7ff5a039d639",   // Seed
                   "8049d639e5a6980d1cd2392abcce41029cda74a1563523a202f09641cc2618f8",   // Public key
                   "erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx"},    // Bech32 address
 
-         pemData {getCanonicPath("testData/keysValid4.pem"),                                   // File path
+         pemData {getCanonicPath("testData/keysValid4.pem"),                            // File path
                   "e253a571ca153dc2aee845819f74bcc9773b0586edead15a94cb7235a5027436",   // Seed
                   "b2a11555ce521e4944e09ab17549d85b487dcd26c84b5017a39e31a3670889ba",   // Public key
                   "erd1k2s324ww2g0yj38qn2ch2jwctdy8mnfxep94q9arncc6xecg3xaq6mjse8"}));  // Bech32 address
