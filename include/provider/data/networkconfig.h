118551c5620123a901cc20e5b06ad11d32e65a4b
Marius Ciuca <marius.ciucam@gmail.com>
2022-06-21 14:54:46 +0300

    FEAT: Add link to trustwallet repo

--- include/provider/data/networkconfig.h
+++ include/provider/data/networkconfig.h
@@ -3,8 +3,9 @@
 
 #include <string>
 
-/// A "NetworkConfig" object holds the network parameters relevant to creating transactions (e.g. minimum gas limit, minimum gas price).
-/// The following fields can (should) be fetched from https://api.elrond.com/network/config.
+// A "NetworkConfig" object holds the network parameters relevant to creating transactions (e.g. minimum gas limit, minimum gas price).
+// The following fields can (should) be fetched from https://api.elrond.com/network/config.
+// Also, see https://github.com/trustwallet/wallet-core/blob/master/src/Elrond/NetworkConfig.h
 struct NetworkConfig
 {
     std::string chainId;
