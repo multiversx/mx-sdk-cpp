91e0a46084985a36d4188cbd0fa0d97b2aa25fb2
Marius Ciuca <marius.ciucam@gmail.com>
2022-06-27 16:17:35 +0300

    FEAT: Working version of fromAmount with string

--- src/transaction/token_payment.cpp
+++ src/transaction/token_payment.cpp
@@ -1,4 +1,5 @@
 #include <utility>
+#include <algorithm>
 
 #include "transaction/token_payment.h"
 
@@ -6,14 +7,31 @@ namespace internal
 {
 BigUInt bigUIntFromVal(double value, uint32_t numDecimals)
 {
+    std::string valStr = std::to_string(value);
+    auto maxDoubleLen = std::to_string(std::numeric_limits<double>::max()).length();
+    if (valStr.length() >= maxDoubleLen)
+    {
+        valStr.erase(std::remove(valStr.begin(), valStr.end(), '.'), valStr.end());
+        return BigUInt(valStr);
+    }
 
     auto v = value * std::pow(10, numDecimals);
+    auto xxx = std::numeric_limits<uint64_t>::max();
     BigUInt val = BigUInt(std::to_string(uint64_t(v)));
-   // std::string zeroes = std::string(numDecimals, '0');
-  //  BigUInt multiplier = BigUInt("1" + zeroes);
+    // std::string zeroes = std::string(numDecimals, '0');
+    //  BigUInt multiplier = BigUInt("1" + zeroes);
 
     return val;// * multiplier;
 }
+
+bool is_number(const std::string &s)
+{
+    // check first numbers are != 0
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
+
 }
 
 TokenPayment::TokenPayment(std::string tokenIdentifier, uint64_t nonce, BigUInt value, uint32_t numDecimals)
@@ -23,10 +41,46 @@ TokenPayment::TokenPayment(std::string tokenIdentifier, uint64_t nonce, BigUInt 
           m_numDecimals(numDecimals)
 {}
 
-TokenPayment TokenPayment::fungibleFromAmount(std::string tokenIdentifier, double value, uint32_t numDecimals)
+TokenPayment TokenPayment::fungibleFromAmount(std::string tokenIdentifier, std::string value, uint32_t numDecimals)
 {
-    BigUInt amountBigInt = internal::bigUIntFromVal(value, numDecimals);
-    return {std::move(tokenIdentifier), 0, amountBigInt, numDecimals};
+    size_t pointCt = std::count(value.begin(), value.end(), '.');
+    if (pointCt > 1)
+    {
+        //do smth
+    }
+
+    std::string valWithoutPoint = value;
+    valWithoutPoint.erase(std::remove(valWithoutPoint.begin(), valWithoutPoint.end(), '.'), valWithoutPoint.end());
+
+    if (!internal::is_number(valWithoutPoint))
+    {
+        // another err
+    }
+
+    if (pointCt == 1)
+    {
+        auto const dotPosition = value.find_last_of('.');
+        auto first = value.substr(0, dotPosition);
+        first = (first == "0") ? "" : first;
+        auto second = value.substr(dotPosition+1, value.length());
+        if (second.length() > numDecimals)
+        {
+            second = second.substr(0, numDecimals);
+        }
+        else
+        {
+            int zeroesCt = int(numDecimals - second.length());
+            zeroesCt = std::max(zeroesCt, 0);
+            std::string zeroes = std::string(zeroesCt, '0');
+            second += zeroes;
+        }
+        return {std::move(tokenIdentifier), 0, BigUInt(first + second), numDecimals};
+    }
+    std::string zeroes = std::string(numDecimals, '0');
+    return  {std::move(tokenIdentifier), 0, BigUInt(value+ zeroes), numDecimals};
+
+   // BigUInt amountBigInt = internal::bigUIntFromVal(value, numDecimals);
+
 }
 
 TokenPayment TokenPayment::fungibleFromBigUInt(std::string tokenIdentifier, BigUInt value, uint32_t numDecimals)
--- tests/test_src/test_transaction/test_token_payment.cpp
+++ tests/test_src/test_transaction/test_token_payment.cpp
@@ -2,20 +2,26 @@
 
 #include "transaction/token_payment.h"
 
+
 TEST(TokenPayment, withESDT)
 {
     std::string identifier = "USDC-c76f1f";
     size_t numDecimals = 6;
 
-    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, 1, numDecimals).toString(), "1000000");
-    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, 1.1, numDecimals).toString(), "1100000");
-    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, 0.1, numDecimals).toString(), "100000");
-    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, 0.123456789, numDecimals).toString(), "123456");
-    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, 12345.6789, numDecimals).toString(), "12345678900");
+    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, "1", numDecimals).toString(), "1000000");
+    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, "1.1", numDecimals).toString(), "1100000");
+    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, "0.1", numDecimals).toString(), "100000");
+    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, "0.123456789", numDecimals).toString(), "123456");
+    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, "123.456789", numDecimals).toString(), "123456789");
+    EXPECT_EQ(TokenPayment::fungibleFromAmount(identifier, "12345.6789", numDecimals).toString(), "12345678900");
+
     EXPECT_EQ(TokenPayment::fungibleFromBigUInt(identifier, BigUInt("1000000"), numDecimals).toString(), "1000000");
+
+    EXPECT_EQ(TokenPayment::fungibleFromBigUInt(identifier, BigUInt("1"), numDecimals).toPrettyString(), "0.000001 USDC-c76f1f");
     EXPECT_EQ(TokenPayment::fungibleFromBigUInt(identifier, BigUInt("1000000"), numDecimals).toPrettyString(), "1.000000 USDC-c76f1f");
-    EXPECT_EQ(TokenPayment::fungibleFromBigUInt(identifier, BigUInt("123000000"), numDecimals).toString(), "123000000");
     EXPECT_EQ(TokenPayment::fungibleFromBigUInt(identifier, BigUInt("123000000"), numDecimals).toPrettyString(), "123.000000 USDC-c76f1f");
+    EXPECT_EQ(TokenPayment::fungibleFromBigUInt(identifier, BigUInt("12345678"), numDecimals).toPrettyString(), "12.345678 USDC-c76f1f");
     EXPECT_EQ(TokenPayment::fungibleFromBigUInt(identifier, BigUInt("123456789"), numDecimals).toPrettyString(), "123.456789 USDC-c76f1f");
+
     EXPECT_EQ(TokenPayment::fungibleFromBigUInt(identifier, BigUInt("123456789")).toPrettyString(), "123456789.0 USDC-c76f1f"); // without specifying numDecimals
 }
