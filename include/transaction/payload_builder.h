ce6ff4226676a7580d148f17eb85539617d188a6
Marius Ciuca <marius.ciucam@gmail.com>
2022-06-29 14:34:28 +0300

    FEAT: Add comment

--- include/transaction/payload_builder.h
+++ include/transaction/payload_builder.h
@@ -1,6 +1,8 @@
 #ifndef ERD_PAYLOAD_BUILDER_H
 #define ERD_PAYLOAD_BUILDER_H
 
+// This file is an adaptation from https://github.com/ElrondNetwork/elrond-sdk-erdjs/blob/main/src/tokenTransferBuilders.ts
+
 #include "transaction/token_payment.h"
 
 class ESDTTransferPayloadBuilder
@@ -15,6 +17,5 @@ public:
 private:
     TokenPayment m_payment;
 };
-
 
 #endif //ERD_PAYLOAD_BUILDER_H
