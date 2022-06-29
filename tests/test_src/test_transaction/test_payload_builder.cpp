9f94fe84f7e25d64ca6e6673504185b3f20442f1
Marius Ciuca <marius.ciucam@gmail.com>
2022-06-29 14:45:37 +0300

    FEAT: Extra test

--- tests/test_src/test_transaction/test_payload_builder.cpp
+++ tests/test_src/test_transaction/test_payload_builder.cpp
@@ -3,8 +3,12 @@
 
 TEST(ESDTTransferPayloadBuilder, build)
 {
-    TokenPayment const payment = TokenPayment::fungibleFromAmount("COUNTER-8b028f", "100.00", 0);
-    std::string const payload = ESDTTransferPayloadBuilder().setPayment(payment).build();
+    TokenPayment payment = TokenPayment::fungibleFromAmount("COUNTER-8b028f", "100.00", 0);
+    std::string payload = ESDTTransferPayloadBuilder().setPayment(payment).build();
 
     EXPECT_EQ(payload, "ESDTTransfer@434f554e5445522d386230323866@64");
+
+    payment = TokenPayment::fungibleFromAmount("RIDE-7d18e9", "1634.132360763445665862", 18);
+    payload = ESDTTransferPayloadBuilder().setPayment(payment).build();
+    EXPECT_EQ(payload, "ESDTTransfer@524944452d376431386539@589624641c2ba8e046");
 }
