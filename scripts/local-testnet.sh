25800f9d3d1536a7c92b0d7c17ccffbe04ed1cdc
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2022-07-08 14:24:06 +0300

    removed script, added its functionality to local-testnet.sh

--- scripts/local-testnet.sh
+++ scripts/local-testnet.sh
@@ -3,6 +3,7 @@ WORKING_DIR="$CURRENT_DIR"/../..
 TESTNET_DIR=$WORKING_DIR/testnet
 TESTNET_OUTPUT_DIR=$TESTNET_DIR/testnet-local
 SCRIPTS_DIR=elrond-go/scripts/testnet
+VARIABLES_PATH=$SCRIPTS_DIR/variables.sh
 SANDBOX_NAME=sandbox
 
 cloneDependencies(){
@@ -36,10 +37,21 @@ testnetPrereq(){
     bash $SCRIPTS_DIR/config.sh
 }
 
+testnetUpdateVariables(){
+  sed -i 's/export SHARDCOUNT=.*/export SHARDCOUNT=1/' $VARIABLES_PATH
+  sed -i 's/SHARD_VALIDATORCOUNT=.*/SHARD_VALIDATORCOUNT=1/' $VARIABLES_PATH
+  sed -i 's/SHARD_OBSERVERCOUNT=.*/SHARD_OBSERVERCOUNT=1/' $VARIABLES_PATH
+  sed -i 's/SHARD_CONSENSUS_SIZE=.*/SHARD_CONSENSUS_SIZE=$SHARD_VALIDATORCOUNT/' $VARIABLES_PATH
+  sed -i 's/META_VALIDATORCOUNT=.*/META_VALIDATORCOUNT=1/' $VARIABLES_PATH
+  sed -i 's/META_OBSERVERCOUNT=.*/META_OBSERVERCOUNT=1/' $VARIABLES_PATH
+  sed -i 's/META_CONSENSUS_SIZE=.*/META_CONSENSUS_SIZE=$META_VALIDATORCOUNT/' $VARIABLES_PATH
+}
+
 testnetNew(){
   cloneDependencies
   testnetRemove
   testnetSetup
+  testnetUpdateVariables
   testnetPrereq
 }
 
