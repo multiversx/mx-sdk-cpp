d529baa3abd4834a504ac6594d5f1c46dc89bd9a
Marius Ciuca <marius.ciucam@gmail.com>
2022-07-22 10:51:54 +0300

    FIX: Install script

--- install.sh
+++ install.sh
@@ -3,19 +3,29 @@ INCLUDE_PATH=/usr/include/
 LIB_PATH=/usr/lib
 
 echo "Installing libsodium library..."
-sudo apt install libsodium-dev
+sudo apt install libsodium-dev || exit 1
 
 echo "Updating submodules..."
-git submodule update --init --recursive
+git submodule update --init --recursive || exit 1
 
 echo "Cmake"
-cmake .
+cmake . || exit 1
 
 echo "Building solution..."
-cmake --build .
+cmake --build . || exit 1
 
 echo "Installing sdk..."
+
 sudo cp -r "$CURRENT_DIR/include/" "$INCLUDE_PATH/erdcpp"
+if [[ $? -ne 0 ]]; then
+  echo "Could not copy include headers from $CURRENT_DIR/include/ to $INCLUDE_PATH/erdcpp"
+  exit 1
+fi
+
 sudo cp "$CURRENT_DIR/src/libsrc.so" "$LIB_PATH"
+if [[ $? -ne 0 ]]; then
+  echo "Could not copy generated shared library from $CURRENT_DIR/src/libsrc.so to $LIB_PATH"
+  exit 1
+fi
 
 echo "Finished successfully !"
