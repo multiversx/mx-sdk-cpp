d60b70d27200231cea78411d2ea129b0497b6969
Marius Ciuca <marius.ciucam@gmail.com>
2022-07-22 15:52:55 +0300

    FEAT: Add comparison operator + split signer tests

--- tests/test_src/test_transaction/test_transaction.cpp
+++ tests/test_src/test_transaction/test_transaction.cpp
@@ -3,10 +3,8 @@
 #include "utils/hex.h"
 #include "utils/errors.h"
 #include "transaction/esdt.h"
-#include "transaction/signer.h"
 #include "transaction/transaction.h"
 #include "transaction/messagesigner.h"
-#include "wrappers/cryptosignwrapper.h"
 
 // Most tests for signing and transaction construction are adapted from one of the following sources:
 // ERD-JS: https://github.com/ElrondNetwork/elrond-sdk-erdjs/blob/bb926b029150d7c79f2b37308f4334f98a4cabf7/src/testutils/wallets.ts#L110
@@ -14,82 +12,6 @@
 // ERD-PY: https://github.com/ElrondNetwork/elrond-sdk-erdpy/blob/main/erdpy/tests/test_wallet.py
 // ERD-GO: https://github.com/ElrondNetwork/elrond-go/blob/master/examples/construction_test.go
 //         https://github.com/ElrondNetwork/elrond-go/blob/7d133422048b7e65f0a8730cfdd1fff9d51077e9/examples/messageSign_test.go#L87
-
-class SignerConstructorFixture : public ::testing::Test
-{
-public:
-    template <typename ErrorType>
-    void expectException(bytes const &seed, errorMessage const &errMsg)
-    {
-        EXPECT_THROW({
-                         try
-                         {
-                             Signer signer(seed);
-                         }
-                         catch(const ErrorType &e)
-                         {
-                             EXPECT_EQ( errMsg, e.what() );
-                             throw;
-                         }
-                     }, ErrorType );
-    }
-};
-
-TEST_F(SignerConstructorFixture, invalidSeed)
-{
-    bytes const validSeed = util::hexToBytes("1a927e2af5306a9bb2ea777f73e06ecc0ac9aaa72fb4ea3fecf659451394cccf");
-    EXPECT_NO_THROW(Signer signer(validSeed));
-
-    bytes invalidSeed(validSeed);
-    invalidSeed.emplace_back(4);
-
-    expectException<std::length_error>(invalidSeed, ERROR_MSG_KEY_BYTES_SIZE);
-}
-
-TEST(Signer, getSignature)
-{
-    bytes const seed = util::hexToBytes("1a927e2af5306a9bb2ea777f73e06ecc0ac9aaa72fb4ea3fecf659451394cccf");
-    Signer signer(seed);
-
-    std::string msg = "{\"nonce\":0,\"value\":\"0\",\"receiver\":\"erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r\",\"sender\":\"erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz\",\"gasPrice\":1000000000,\"gasLimit\":50000,\"data\":\"Zm9v\",\"chainID\":\"1\",\"version\":1}";
-    std::string signature = signer.getSignature(msg);
-    std::string expectedSignature = "b5fddb8c16fa7f6123cb32edc854f1e760a3eb62c6dc420b5a4c0473c58befd45b621b31a448c5b59e21428f2bc128c80d0ee1caa4f2bf05a12be857ad451b00";
-
-    EXPECT_EQ(util::stringToHex(signature), expectedSignature);
-}
-
-TEST(Signer, verify_signature_by_signer)
-{
-    bytes const seed = util::hexToBytes("1a927e2af5306a9bb2ea777f73e06ecc0ac9aaa72fb4ea3fecf659451394cccf");
-    Signer signer(seed);
-
-    std::string msg = "{\"nonce\":0,\"value\":\"0\",\"receiver\":\"erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r\",\"sender\":\"erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz\",\"gasPrice\":1000000000,\"gasLimit\":50000,\"data\":\"Zm9v\",\"chainID\":\"1\",\"version\":1}";
-    std::string signature = util::hexToString("b5fddb8c16fa7f6123cb32edc854f1e760a3eb62c6dc420b5a4c0473c58befd45b621b31a448c5b59e21428f2bc128c80d0ee1caa4f2bf05a12be857ad451b00");
-
-    EXPECT_TRUE(signer.verify(signature, msg));
-}
-
-TEST(Signer, verify_signature_by_publicKey)
-{
-    bytes const seed = util::hexToBytes("1a927e2af5306a9bb2ea777f73e06ecc0ac9aaa72fb4ea3fecf659451394cccf");
-    bytes const secretKey = wrapper::crypto::getSecretKey(seed);
-    bytes const publicKey = wrapper::crypto::getPublicKey(secretKey);
-
-    std::string msg = "{\"nonce\":0,\"value\":\"0\",\"receiver\":\"erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r\",\"sender\":\"erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz\",\"gasPrice\":1000000000,\"gasLimit\":50000,\"data\":\"Zm9v\",\"chainID\":\"1\",\"version\":1}";
-    std::string signature = util::hexToString("b5fddb8c16fa7f6123cb32edc854f1e760a3eb62c6dc420b5a4c0473c58befd45b621b31a448c5b59e21428f2bc128c80d0ee1caa4f2bf05a12be857ad451b00");
-
-    EXPECT_TRUE(Signer::verify(signature, msg, publicKey));
-}
-
-TEST(Signer, verify_signature_by_address)
-{
-    Address signerAddr("erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz");
-
-    std::string msg = "{\"nonce\":8,\"value\":\"10000000000000000000\",\"receiver\":\"erd1cux02zersde0l7hhklzhywcxk4u9n4py5tdxyx7vrvhnza2r4gmq4vw35r\",\"sender\":\"erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz\",\"gasPrice\":1000000000,\"gasLimit\":50000,\"chainID\":\"1\",\"version\":1}";
-    std::string signature = util::hexToString("4a6d8186eae110894e7417af82c9bf9592696c0600faf110972e0e5310d8485efc656b867a2336acec2b4c1e5f76c9cc70ba1803c6a46455ed7f1e2989a90105");
-
-    EXPECT_TRUE(Signer::verify(signature, msg, signerAddr));
-}
 
 struct signSerializedTxData
 {
